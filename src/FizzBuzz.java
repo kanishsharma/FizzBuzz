3a9586f8cfa50082de08703103cd940b9bd60379
unknown <kanish.sharma@mail.utoronto.ca>
2023-09-12 10:16:01 -0400

    refactored to extract helper method

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -5,18 +5,18 @@ class FizzBuzz {
 
     public static void main(String[] args) {
 
-        for (int i = 1; i < 100; i++) {
+        for (int i = 1; i < 100; i++) { // essentially three conditions set in one line compared to Python
 
             // Find out which numbers divide i.
-            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy3 = i % 3 == 0; // Functions seem to be defined with = exclusively instead of a bdoy
             boolean divisibleBy5 = i % 5 == 0;
 
             // Print our appropriate result.
-            if (divisibleBy3 && divisibleBy5) {
+            if (divisibleBy3 && divisibleBy5) { // The "and" statement is represented by &&
 
-                System.out.println("Fizz Buzz");
+                System.out.println("Fizz Buzz"); // print() is a more specific function in Java
 
-            } else if (divisibleBy3) {
+            } else if (divisibleBy3) { // Instead of using tabs or spaces, you need to use {} for functions?
 
                 System.out.println("Fizz");
 
